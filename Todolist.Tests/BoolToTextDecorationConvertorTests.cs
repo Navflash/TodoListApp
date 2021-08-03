d06ed8c8f9cf86479f5735ea79555980bf7cadf4
Navflash <rjnavneetsgr29@gmail.com>
2021-08-03 13:12:02 +0530

    adding test

---
+++ Todolist.Tests/BoolToTextDecorationConvertorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TodoList.Helpers;
+using Xamarin.Forms;
+
+namespace Todolist.Tests
+{
+    public class BoolToTextDecorationConvertorTests
+    {
+        [Fact]
+        public void Convert_returns_strikethrough_when_item_is_complelted()
+        {
+            //Arrange
+            var convertor = new BoolToTextDecoration();
+            //Act
+            var result = convertor.Convert(true, null, null, null);
+            //Assert
+            Assert.Equal(TextDecorations.Strikethrough, (TextDecorations)result);
+        }
+
+        [Fact]
+        public void Convert_returns_none_when_item_is_not_complelted()
+        {
+            //Arrange
+            var convertor = new BoolToTextDecoration();
+            //Act
+            var result = convertor.Convert(false, null, null, null);
+            //Assert
+            Assert.Equal(TextDecorations.None, (TextDecorations)result);
+        }
+    }
+}
---
+++ Todolist.Tests/TodoViewModelTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TodoList.ViewModel;
+using TodoList.Services;
+
+namespace Todolist.Tests
+{
+    public class TodoViewModelTests
+    {
+        [Fact]
+        public void ViewModel_populates_items_correctly()
+        {
+            TodoViewModel viewModel = new TodoViewModel(new DialogMessageMock());
+
+            Assert.Equal(4, viewModel.Items.Count);
+        }
+
+        [Fact]
+        public void AddItemCommand_adds_new_item_to_the_list()
+        {
+            var dialogMessage = new DialogMessageMock();
+            dialogMessage.DialogResponse = "Todo Item 5";
+            var viewModel = new TodoViewModel(dialogMessage);
+
+            viewModel.AddItemCommand.Execute(null);
+
+            Assert.Equal(5, viewModel.Items.Count);
+            Assert.Equal("Todo Item 5", viewModel.Items.Last().Name);
+        }
+        [Fact]
+        public void MarkAsCompletedCommand_marks_item_as_completed_and_puts_it_at_the_end_of_list()
+        {
+            var viewModel = new TodoViewModel(new DialogMessageMock());
+
+            viewModel.MarkAsCompletedCommand.Execute(viewModel.Items.First());
+
+            Assert.True(viewModel.Items.Last().Completed);
+        }
+
+        [Fact]
+        public void MarkAsCompletedCommand_update_progress()
+        {
+            var viewModel = new TodoViewModel(new DialogMessageMock());
+
+            viewModel.MarkAsCompletedCommand.Execute(viewModel.Items.First());
+
+            Assert.Equal("Completed 1/4", viewModel.CompletedHeader);
+            Assert.Equal(0.25, viewModel.CompletedProgress);
+        }
+
+        class DialogMessageMock : IDialogMessage
+        {
+            public int DisplayAlertCallCount { get; set; }
+            public string DialogResponse { get; set; }
+
+            public Task DisplayAlert(string title, string message, string cancel)
+            {
+                DisplayAlertCallCount++;
+                return Task.CompletedTask;
+            }
+
+            public Task<string> DisplayActionSheet(string title, string destruction, params string[] buttons)
+            {
+                return Task.FromResult(DialogResponse);
+            }
+
+
+            public Task<string> DisplayPrompt(string title, string message)
+            {
+                return Task.FromResult(DialogResponse);
+            }
+        }
+    }
+}
